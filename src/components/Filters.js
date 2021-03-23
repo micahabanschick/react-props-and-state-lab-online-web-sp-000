69085517c18de619261fef8e098a08d68d35bf42
Micah Banschick <micha.ban@gmail.com>
2021-03-23 16:53:00 +0000

    Automatically backed up by Learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -20,7 +20,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select onChange={event => this.handleChange(event)} name="type" id="type" value={this.state.select}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -29,7 +29,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button onClick={this.handleClick} className="ui secondary button">Find pets</button>
         </div>
       </div>
     )
